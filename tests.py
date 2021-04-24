1f2e453e1f60df258fd24442a32dbbdee3a54e0e
Justin Boyer <justinbboyer@gmail.com>
2021-04-24 12:08:01 -0700

    Add test class

--- tests.py
+++ tests.py
@@ -2,12 +2,13 @@ import unittest
 from contrived_func import contrived_func
 
 
-def test1(self):
-    self.assertTrue(contrived_func(111))
+class Test(unittest.TestCase):
 
+    def test1(self):
+        self.assertTrue(contrived_func(111))
 
-def test2(self):
-    self.assertFalse(contrived_func(6))
+    def test2(self):
+        self.assertFalse(contrived_func(6))
 
 
 if __name__ == '__main__':
